3d12a025d688943e90a71ffa88cd0964233ea959
PirateRobe <177021961+PirateRobe@users.noreply.github.com>
2025-07-29 11:47:37 +0200

    Update booking_details_screen.dart

--- booking_details_screen.dart
+++ booking_details_screen.dart
@@ -1,8 +1,3 @@
-/*
- * Student Numbers: 222034621, 221019559, 223083143, 223004229, 222074359, 223051502, 223021893
- * Student Names  : Temeki K, Maduna R, Ngwenya OL, Serebolo R, Rampedi MO, Masabala T, Sebolai KL
- * Question       : BookingDetailScreen
- */
 
 import 'package:final_test/models/student_booking_model.dart';
 import 'package:final_test/routes/route_manager.dart';
