f9bda56e51c4b587ae29edbe27f24374eb087fb0
PirateRobe <177021961+PirateRobe@users.noreply.github.com>
2025-07-29 11:48:32 +0200

    Update login_screen.dart

--- login_screen.dart
+++ login_screen.dart
@@ -1,8 +1,3 @@
-/*
- * Student Numbers: 222034621, 221019559, 223083143, 223004229, 222074359, 223051502, 223021893
- * Student Names  : Temeki K, Maduna R, Ngwenya OL, Serebolo R, Rampedi MO, Masabala T, Sebolai KL
- * Question       : LoginScreen
- */
 
 import 'package:final_test/routes/route_manager.dart';
 import 'package:final_test/viewmodels/auth_view_model.dart';
