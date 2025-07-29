2efe2b6dc257b506a627ba45b7e26395a82ab9f3
PirateRobe <177021961+PirateRobe@users.noreply.github.com>
2025-07-29 11:49:20 +0200

    Update register_screen.dart

--- register_screen.dart
+++ register_screen.dart
@@ -1,8 +1,4 @@
-/*
- * Student Numbers: 222034621, 221019559, 223083143, 223004229, 222074359, 223051502, 223021893
- * Student Names  : Temeki K, Maduna R, Ngwenya OL, Serebolo R, Rampedi MO, Masabala T, Sebolai KL
- * Question       : RegisterScreen
- */
+
 
 import 'package:final_test/viewmodels/auth_view_model.dart';
 import 'package:final_test/views/admin_dashboard_screen.dart';
