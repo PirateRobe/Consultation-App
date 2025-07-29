c85ac1b4f53b2f6d4cd5587212b15fe9aa270c0f
PirateRobe <177021961+PirateRobe@users.noreply.github.com>
2025-07-29 11:48:19 +0200

    Update home_screen.dart

--- home_screen.dart
+++ home_screen.dart
@@ -1,8 +1,4 @@
-/*
- * Student Numbers: 222034621, 221019559, 223083143, 223004229, 222074359, 223051502, 223021893
- * Student Names  : Temeki K, Maduna R, Ngwenya OL, Serebolo R, Rampedi MO, Masabala T, Sebolai KL
- * Question       : HomeScreen
- */
+
 
 import 'package:final_test/models/student_booking_model.dart'; // Import the booking model
 import 'package:final_test/routes/route_manager.dart'; // Import route manager for navigation
